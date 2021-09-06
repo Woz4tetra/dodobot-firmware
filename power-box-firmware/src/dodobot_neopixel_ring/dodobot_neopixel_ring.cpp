0538059c8606d0778d7e76fd6ee23a0d92a65293
Woz4tetra <woz4tetra@gmail.com>
2021-09-06 02:09:50 -0400

    switching ring light command to use colors directly

--- power-box-firmware/src/dodobot_neopixel_ring/dodobot_neopixel_ring.cpp
+++ power-box-firmware/src/dodobot_neopixel_ring/dodobot_neopixel_ring.cpp
@@ -24,25 +24,12 @@ void DodobotNeopixelRing::off()
 
 void DodobotNeopixelRing::packet_callback()
 {
-    pixels->clear();
-    int32_t pattern_type;
-    CHECK_SEGMENT(serial, serial->segment_as_int32(pattern_type));
-    
     uint32_t color;
-    int pattern_delay = 0;
-    switch (pattern_type)
-    {
-        case 1: color = pixels->Color(255, 255, 255, 255); break;
-        case 2: color = pixels->Color(0, 255, 0, 20); pattern_delay = 10; break;
-        default: color = pixels->Color(0, 0, 0, 0); break;
-    }
-    for (int i = 0; i < num_pixels; i++)
-    {
+    CHECK_SEGMENT(serial, serial->segment_as_uint32(color));
+    
+    pixels->clear();
+    for (int i = 0; i < num_pixels; i++) {
         pixels->setPixelColor(i, color);
-        if (delay > 0) {
-            pixels->show();
-            delay(pattern_delay);
-        }
     }
     pixels->show();
 }
